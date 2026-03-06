82b1a661601f5facc02a8e2f695d052b1e2807c4
Nazmul Alam Efti <152446368+Efti6181@users.noreply.github.com>
2026-03-07 01:49:54 +0600

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,11 @@
 <?php
 
      $con = mysqli_connect("localhost","root","","umsdb");
-
-// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+
 
 
 ?>
